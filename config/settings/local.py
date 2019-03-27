13f45ad7b80391d52df6fd3c03551cb268a9c297
Aayush Nandkeolyar <nandkeolyar.aayush@gmail.com>
2019-03-28 00:32:47 +0530

    Add AWS IP

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="OF2TevfztfhUkPrjloCgtddVtX6ioDh2sKhmh3WXf3PwRC6UeUGlQH9juZLH22Bc",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "13.232.204.63"]
 
 # CACHES
 # ------------------------------------------------------------------------------
